802cc92f5ceea9a24615ba46a778c83aa660f9f3
elborracho420 <e.baxter@dark-coin.io>
2022-09-25 16:58:02 -0500

    Updated notes on palindrome checker and list of regex notes

--- js-algorithm-data-structure-projects/palindrome-checker/palindrome.js
+++ js-algorithm-data-structure-projects/palindrome-checker/palindrome.js
@@ -17,3 +17,11 @@ function palindrome(str) {
 console.log(palindrome("123211"))
 
 palindrome("eye");
+
+//how to match only alphanumeric characters in various quotes and strings:
+// [\w] is short for [A-Za-z0-9_]
+// regex to match all letters and numbers
+
+/* how to match the opposite of alphanumerics (non alphanumerics)
+   [\W] is short for [^A-Za-z0-9_]
+   note this is a capital W 
