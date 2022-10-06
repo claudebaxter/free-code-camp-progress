82bad6aa85d2b71da2d2be24599042d167f964a2
elborracho420 <e.baxter@dark-coin.io>
2022-10-06 14:24:48 -0500

    Updating notes on basic algorithm scripting module, how to search a sentence for the string with longest length.

--- js-algorithm-data-structure-projects/js-notes/basic-algorithm-scripting.js
+++ js-algorithm-data-structure-projects/js-notes/basic-algorithm-scripting.js
@@ -131,3 +131,85 @@ factorialize(5);
 
 //--------------------------------------------------
 
+/*Find the Longest Word in a String
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.*/
+
+function findLongestWordLength(str) {
+  return str.length;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//SOLUTION:
+
+function findLongestWordLength(str) {
+  let words = str.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+  return maxLength;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+//alternative solution 1:
+function findLongestWordLength(s) {
+  return s.split(' ')
+    .reduce(function(longest, word) {
+      return Math.max(longest, word.length)
+    }, 0);
+}
+
+//alt sol 2:
+function findLongestWordLength(str) {
+  return Math.max(...str.split(" ").map(word => word.length));
+}
+
+//alt sol 3:
+function findLongestWordLength(str) {
+  // split the string into individual words
+  const words = str.split(" ");
+
+  // words only has 1 element left that is the longest element
+  if (words.length == 1) {
+    return words[0].length;
+  }
+
+  // if words has multiple elements, remove the first element
+  // and recursively call the function
+  return Math.max(
+    words[0].length,
+    findLongestWordLength(words.slice(1).join(" "))
+  );
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//alt sol 4:
+function findLongestWordLength(str) {
+let longestLength = 0;
+let currentLength = 0;
+
+for (let i = 0; i < str.length; i++) {
+  if (str[i] === " ") {
+    if (currentLength > longestLength) longestLength = currentLength;
+    currentLength = 0;
+  } else {
+    currentLength++;
+  }
+}
+if (currentLength > longestLength) {
+  longestLength = currentLength;
+}
+
+return longestLength;
+}
+
+//-------------------------------------------------------
+
