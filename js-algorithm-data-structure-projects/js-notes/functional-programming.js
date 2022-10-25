bd34b4d81f3120939006c0ffdf2ff28f1e90614e
elborracho420 <e.baxter@dark-coin.io>
2022-10-25 18:43:33 -0500

    How to implement map on a prototype.

--- js-algorithm-data-structure-projects/js-notes/functional-programming.js
+++ js-algorithm-data-structure-projects/js-notes/functional-programming.js
@@ -771,3 +771,83 @@ const ratings = watchList.map(({ Title: title, imdbRating: rating }) => ({title,
 
 //----------------------------------------------------------------------
 
+/*Implement map on a Prototype
+As you have seen from applying Array.prototype.map(), or simply map() earlier, the map method returns an array of the same length as the one it was called on. It also doesn't alter the original array, as long as its callback function doesn't.
+
+In other words, map is a pure function, and its output depends solely on its inputs. Plus, it takes another function as its argument.
+
+You might learn a lot about the map method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
+
+Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.*/
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+//SOLUTION:
+
+// The global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+
+  // Add your code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+  // Add your code above this line
+
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+//forEach Solution:
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+  // Add your code below this line
+  this.forEach(a => newArray.push(callback(a)));
+  // Add your code above this line
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+//RECURSION SOLUTION:
+
+// The global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback, arr = [], i = 0) {
+  return i < this.length
+    ? this.myMap(callback, arr.concat(callback(this[i])), i + 1)
+    : arr;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+//-------------------------------------------------------------------------------
+
+
