2d56f1bb2cea48c582c95a92a0315b35317efa39
elborracho420 <e.baxter@dark-coin.io>
2022-10-25 20:27:31 -0500

    How to implement the filter method on a prototype.

--- js-algorithm-data-structure-projects/js-notes/functional-programming.js
+++ js-algorithm-data-structure-projects/js-notes/functional-programming.js
@@ -1034,3 +1034,66 @@ const filteredList = watchList
 console.log(filteredList);
 
 //------------------------------------------------------------------------------------
+
+/*Implement the filter Method on a Prototype
+You might learn a lot about the filter method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
+
+Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.*/
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  const newArray = [];
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
+
+//SOLUTION:
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) === true) {
+      newArray.push(this[i]);
+    }
+  }
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
+
+//ALT SOL:
+
+// The global variable
+let s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  // Only change code below this line
+  let newArray = [];
+  this.forEach(function(x) {
+    if (callback(x) == true) {
+      newArray.push(x);
+    }
+  });
+  // Only change code above this line
+  return newArray;
+};
+
+let new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
+
+//--------------------------------------------------------------------
