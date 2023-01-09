d35fb31246fd5b1c66597f82707970f689922962
elborracho420 <e.baxter@dark-coin.io>
2023-01-09 12:07:03 -0600

    Reducing repitition by mapping object arrays and tweaked CSS.

--- front-end-development-libraries/Test-Projects/Javascript-Calculator/src/App.js
+++ front-end-development-libraries/Test-Projects/Javascript-Calculator/src/App.js
@@ -1,44 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const App = () => {
+  const [operatorButtons] = useState([
+    { name: "clear", id: "AC", key: 1 },
+    { name: "equals", id: "=", key: 2 },
+    { name: "add", id: "+", key: 3 },   
+    { name: "subtract", id: "-", key: 4 }, 
+    { name: "multiply", id: "X", key: 5 },
+    { name: "divide", id: "/", key: 6 },
+    { name: "decimal", id: ".", key: 7 }
+  ]);
 
-    }
-  }
+  const [numberButtons] = useState([
+    { name: "zero", id: 0 },
+    { name: "one", id: 1 },
+    { name: "two", id: 2 },
+    { name: "three", type: "button", id: 3 },
+    { name: "four", id: 4 },
+    { name: "five", id: 5 },
+    { name: "six", id: 6 },
+    { name: "seven", id: 7 },
+    { name: "eight", id: 8 },
+    { name: "nine", id: 9 }
+  ]);
 
-  render() {
   return (
     <div className="Calculator">
-      <div id="display"></div>
+      <div id="display">123456789</div>
       <div id="operator-pad">
-        <button id="clear"></button>
-        <button id="equals">=</button>
-        <button id="add">+</button>
-        <button id="subtract">-</button>
-        <button id="multiply">X</button>
-        <button id="divide">/</button>
-        <button id="decimal">.</button>
-
+      {operatorButtons.map(operatorButton => (
+        <button className="button" key={operatorButton.key} id={ operatorButton.name }>{ operatorButton.id }</button> 
+      ))}
       </div>
       <div id="number-pad">
-        <button id="zero">0</button>
-        <button id="one">1</button>
-        <button id="two">2</button>
-        <button id="three">3</button>
-        <button id="four">4</button>
-        <button id="five">5</button>
-        <button id="six">6</button>
-        <button id="seven">7</button>
-        <button id="eight">8</button>
-        <button id="nine">9</button>
+      {numberButtons.map(numberButton => (
+        <button className="button" key={numberButton.id} id={ numberButton.name }>{ numberButton.id }</button>
+      ))}
       </div>
-      
     </div>
   );
-  }
 }
 
 export default App;
