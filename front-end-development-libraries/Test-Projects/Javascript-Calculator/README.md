619f0353e682f7b1ba279650cbc842f487923a37
elborracho420 <e.baxter@dark-coin.io>
2023-01-15 22:45:46 -0600

    Downgraded to react 17 due to testing suite error, passing all tests now.

--- front-end-development-libraries/Test-Projects/Javascript-Calculator/README.md
+++ front-end-development-libraries/Test-Projects/Javascript-Calculator/README.md
@@ -2,4 +2,4 @@
 
 This is my submission for the freecodecamp.org Javascript Calculator Challenge.
 
-To view the Javascript Calculator, click<a href=https://javascript-calculator-seven-lake.vercel.app/ title="Javascript Calculator"> here.</a>
+To view the Javascript Calculator, click<a href=https://fcc-calculator-navy.vercel.app/ title="Javascript Calculator"> here.</a>
--- front-end-development-libraries/Test-Projects/Javascript-Calculator/src/index.js
+++ front-end-development-libraries/Test-Projects/Javascript-Calculator/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);*/
