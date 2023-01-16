cafb81a9ead8653446daba117cfc6c5f3b3234e5
elborracho420 <e.baxter@dark-coin.io>
2023-01-16 16:45:13 -0600

    Have refresh, increment and decrement buttons functioning and updated README.

--- front-end-development-libraries/Test-Projects/25-5-Clock/README.md
+++ front-end-development-libraries/Test-Projects/25-5-Clock/README.md
@@ -2,6 +2,4 @@
 
 This is my submission for the freecodecamp.org 25-5 Clock Challenge.
 
-To view the 25-5 Clock built with react using state hooks, click<a href=https://fcc-calculator-navy.vercel.app/ title="Javascript Calculator"> here.</a>
-
-(the link doesn't work yet, just a placeholder)
+To view the 25-5 Clock built with react using state hooks, click<a href=https://25-5-clock-elborracho420.vercel.app/ title="FCC 25 + 5 Clock"> here.</a>
--- front-end-development-libraries/Test-Projects/25-5-Clock/src/App.js
+++ front-end-development-libraries/Test-Projects/25-5-Clock/src/App.js
@@ -4,11 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRefresh, faPlay, faPause, faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
-  const [sessionLength, setSessionLength] = useState("25"); //will toggle up/down to set sessionTime
-  const [breakLength, setBreakLength] = useState("5"); //will toggle up/down to set breakTime
+  const [sessionLength, setSessionLength] = useState(25); //will toggle up/down to set sessionTime
+  const [breakLength, setBreakLength] = useState(5); //will toggle up/down to set breakTime
   const [sessionTime, setSessionTime] = useState("25:00"); // session countdown timer
   const [breakTime, setBreakTime] = useState("05:00"); // breaktime countdown timer
   const [timerLabel, setTimerLabel] = useState("Session"); //will set timer label as Session or Break
+
+  const resetTime = () => {
+    setSessionLength(25);
+    setBreakLength(5);
+    setSessionTime("25:00");
+    setBreakTime("05:00");
+  };
+
+  const playPause = () => {};
+
+  const sessionIncrement = () => setSessionLength(sessionLength + 1);
+
+  const sessionDecrement = () => setSessionLength(sessionLength - 1);
+
+  const breakIncrement = () => setBreakLength(breakLength + 1);
+
+  const breakDecrement = () => setBreakLength(breakLength - 1);
 
   return (
     <div className="body">
@@ -18,15 +35,15 @@ const App = () => {
       <div className="switch-wrappers">
         <div className="toggle-switch">
           <h2 id="session-label">Session Length</h2>
-            <FontAwesomeIcon className="arrow" id="session-increment" icon={faArrowUp} style={{ color: "red" }}></FontAwesomeIcon>
+            <FontAwesomeIcon className="arrow" id="session-increment" icon={faArrowUp} style={{ color: "red" }} onClick={() => sessionIncrement()}></FontAwesomeIcon>
             <h1 id="session-length">{sessionLength}</h1>
-            <FontAwesomeIcon className="arrow" id="session-decrement" icon={faArrowDown} style={{ color: "red" }}></FontAwesomeIcon>
+            <FontAwesomeIcon className="arrow" id="session-decrement" icon={faArrowDown} style={{ color: "red" }} onClick={() => sessionDecrement()}></FontAwesomeIcon>
         </div>
         <div className="toggle-switch">
           <h2 id="break-label">Break Length</h2>
-            <FontAwesomeIcon className="arrow" id="break-increment" icon={faArrowUp} style={{ color: "red" }}></FontAwesomeIcon>
+            <FontAwesomeIcon className="arrow" id="break-increment" icon={faArrowUp} style={{ color: "red" }} onClick={() => breakIncrement()}></FontAwesomeIcon>
               <h1 id="break-length">{breakLength}</h1>
-            <FontAwesomeIcon className="arrow" id="break-decrement" icon={faArrowDown} style={{ color: "red" }}></FontAwesomeIcon>
+            <FontAwesomeIcon className="arrow" id="break-decrement" icon={faArrowDown} style={{ color: "red" }} onClick={() => breakDecrement()}></FontAwesomeIcon>
         </div>
       </div>
       <div className="timer-wrapper">
@@ -40,11 +57,11 @@ const App = () => {
         </div>
       </div>
       <div className="control-wrapper">
-        <div id="start_stop">
+        <div id="start_stop" onClick={() => playPause()}>
           <FontAwesomeIcon id="break-decrement" icon={faPlay} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
           <FontAwesomeIcon id="break-decrement" icon={faPause} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
         </div>
-        <div id="reset">
+        <div id="reset" onClick={() => resetTime()}>
           <FontAwesomeIcon id="break-decrement" icon={faRefresh} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
         </div>
       </div>
