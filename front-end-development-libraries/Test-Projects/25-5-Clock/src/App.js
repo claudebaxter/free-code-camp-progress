9cc6d2e2860d2bba89333513a31a6b8e38b4f8e0
elborracho420 <e.baxter@dark-coin.io>
2023-01-17 19:35:29 -0600

    Added functions to make timer work. Passing more test but still buggy.

--- front-end-development-libraries/Test-Projects/25-5-Clock/src/App.js
+++ front-end-development-libraries/Test-Projects/25-5-Clock/src/App.js
@@ -6,26 +6,96 @@ import { faRefresh, faPlay, faPause, faArrowUp, faArrowDown } from '@fortawesome
 const App = () => {
   const [sessionLength, setSessionLength] = useState(25); //will toggle up/down to set sessionTime
   const [breakLength, setBreakLength] = useState(5); //will toggle up/down to set breakTime
-  const [sessionTime, setSessionTime] = useState("25:00"); // session countdown timer
-  const [breakTime, setBreakTime] = useState("05:00"); // breaktime countdown timer
-  const [timerLabel, setTimerLabel] = useState("Session"); //will set timer label as Session or Break
+  const [sessionTime, setSessionTime] = useState(1500); // session countdown timer
+  const [timerLabel, setTimerLabel] = useState("SESSION"); //will set timer label as Session or Break
+  const [play, setPlay] = useState(false); //determines if play function is active or off with a boolean
 
-  const resetTime = () => {
-    setSessionLength(25);
+  const timeout = setTimeout(() => {
+    if(sessionTime && play){
+      setSessionTime(sessionTime - 1);
+    }
+  }, 1000);
+
+  const sessionIncrement = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+      setSessionTime(sessionTime + 60);
+    }
+  }
+
+  const sessionDecrement = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+      setSessionTime(sessionTime - 60);
+    } 
+  }
+
+  const breakIncrement = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+  }
+
+  const breakDecrement = () => {
+    if (breakLength > 1){
+      setBreakLength(breakLength - 1);
+    }
+  }
+
+  const handleReset = () => {
+    clearTimeout(timeout);
+    setPlay(false);
+    setSessionTime(1500);
     setBreakLength(5);
-    setSessionTime("25:00");
-    setBreakTime("05:00");
+    setSessionLength(25);
+    setTimerLabel("SESSION");
+    const audio = document.getElementById("beep");
+    audio.pause()
+    audio.currentTime = 0;
   };
 
-  const playPause = () => {};
+  const playPause = () => {
+    clearTimeout(timeout);
+    setPlay(!play);
+  };
 
-  const sessionIncrement = () => setSessionLength(sessionLength + 1);
+  const resetTimer = () => {
+    const audio = document.getElementById("beep");
+    if(!sessionTime && timerLabel === "SESSION"){
+      setSessionTime(breakLength * 60)
+      setTimerLabel("BREAK")
+      audio.play()
+    }
+    if(!sessionTime && timerLabel === "BREAK"){
+      setSessionTime(sessionLength * 60)
+      setTimerLabel("SESSION")
+      audio.pause()
+      audio.currentTime = 0;
+    }
+  }
 
-  const sessionDecrement = () => setSessionLength(sessionLength - 1);
+  const clock = () => {
+    if(play){
+      timeout
+      resetTimer()
+    }else {
+      clearTimeout(timeout)
+    }
+  }
 
-  const breakIncrement = () => setBreakLength(breakLength + 1);
+  React.useEffect(() => {
+    clock()
+  }, [play, sessionTime, timeout])
 
-  const breakDecrement = () => setBreakLength(breakLength - 1);
+  const timeFormatter = () => {
+    const minutes = Math.floor(sessionTime / 60);
+    const seconds = sessionTime - minutes * 60;
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+
+  const title = timerLabel === "SESSION" ? "Session" : "Break";
 
   return (
     <div className="body">
@@ -49,22 +119,27 @@ const App = () => {
       <div className="timer-wrapper">
         <div id="timer">
           <div id="timer-label">
-            <h2>{timerLabel}</h2>
+            <h2>{title}</h2>
           </div>
-          <div id="time-left">
-            <h1 className="time-left-title">{sessionTime}</h1>
+          <div id="time-left-wrapper">
+            <h1 className="time-left">{timeFormatter()}</h1>
           </div>
         </div>
       </div>
       <div className="control-wrapper">
         <div id="start_stop" onClick={() => playPause()}>
-          <FontAwesomeIcon id="break-decrement" icon={faPlay} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
-          <FontAwesomeIcon id="break-decrement" icon={faPause} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
+          <FontAwesomeIcon id="play-btn" icon={faPlay} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
+          <FontAwesomeIcon id="pause-btn" icon={faPause} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
         </div>
-        <div id="reset" onClick={() => resetTime()}>
-          <FontAwesomeIcon id="break-decrement" icon={faRefresh} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
+        <div id="reset" onClick={() => handleReset()}>
+          <FontAwesomeIcon id="refresh-btn" icon={faRefresh} style={{ color: "red", height: "2em" }}></FontAwesomeIcon>
         </div>
       </div>
+      <audio
+        id="beep" 
+        preload="auto"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      />
     </div>
   );
 }
