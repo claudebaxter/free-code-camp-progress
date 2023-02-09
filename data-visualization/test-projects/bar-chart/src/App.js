0a290d3a062fbd67e10e4f1dce3814a55311677f
elborracho420 <e.baxter@dark-coin.io>
2023-02-08 18:31:58 -0600

    Setting up bar chart component, trying to import data as state.

--- data-visualization/test-projects/bar-chart/src/App.js
+++ data-visualization/test-projects/bar-chart/src/App.js
@@ -1,17 +1,31 @@
+import React, { Component } from 'react'
 import './App.css';
 import * as d3 from "d3";
+import BarChart from './BarChart'
 
-console.log(d3.json(
-  'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
-));
 
-function App() {
-  return (
-    <div className="bar-chart">
-      <header className="chart-header">
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataset: d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+    }
+  }
+
+  render() {
+     console.log(this.state.dataset);
+    return (
+      <div className="App">
+        <div id="title">
+          <h2>United States GDP</h2>
+        </div>
+        <div>
+          <BarChart data={[5,10,1,3]} size={[500,500]} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
