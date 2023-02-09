0909355c8f8b96206f2b3b7cd586a31414a52c44
elborracho420 <e.baxter@dark-coin.io>
2023-02-09 14:44:41 -0600

    Adding comments to explain App.js structure.

--- data-visualization/test-projects/bar-chart/src/App.js
+++ data-visualization/test-projects/bar-chart/src/App.js
@@ -5,10 +5,16 @@ function App() {
   const [data] = useState([200, 250, 60, 150, 175]);
   const svgRef = useRef();
 
+  //anything written in useEffect will be relevant to D3 code
   useEffect(() => {
-
+    // 1 set up svg container
+    // 2 set up scaling
+    // 3 set up the axis
+    // 4 set up the svg data
   }, [data]);
-
+  //passing svgRef to svg lets svg control the DOM
+  //react and D3 both control the DOM, so we are using useRef
+  //to allow D3 to control the DOM.
   return (
     <div className="App">
       <svg ref={svgRef}></svg>
