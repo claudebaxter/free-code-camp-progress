0f51efa1742aab166ce366f67ca1bc7dbaebb2d7
elborracho420 <e.baxter@dark-coin.io>
2023-02-10 16:27:00 -0600

    Trying to fetch and map data from online json data.

--- data-visualization/test-projects/bar-chart/src/App.js
+++ data-visualization/test-projects/bar-chart/src/App.js
@@ -3,11 +3,21 @@ import * as d3 from 'd3';
 import './App.css';
 
 function App() {
-  const [data] = useState([200, 250, 60, 150, 100, 175]);
+  const [countryData, setCountryData] = useState([]);
   const svgRef = useRef();
 
   //anything written in useEffect will be relevant to D3 code
   useEffect(() => {
+
+    //fetch JSON data
+    async function fetchData() {
+      const response = await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json");
+      const data = await response.json();
+      console.log(data);
+      setCountryData(data);
+    }
+    fetchData();
+
     // 1 set up svg container
     const w = 400;
     const h = 300;
@@ -16,17 +26,19 @@ function App() {
       .attr('height', h)
       .style('overflow', 'visible')
       .style('margin-top', '75px');
+
     // 2 set up scaling
     const xScale = d3.scaleBand()
-      .domain(data.map((val, i) => i)) //maps x scale to show # of data points desired
+      .domain(countryData.map((val, i) => i)) //maps x scale to show # of data points desired
       .range([0, w])
       .padding(0.5);
     const yScale = d3.scaleLinear()
       .domain([0, h])
       .range([h, 0]); //inverting range, since y scale always starts from the top left
+
     // 3 set up the axis
     const xAxis = d3.axisBottom(xScale)
-      .ticks(data.length);
+      .ticks(countryData.length);
     const yAxis = d3.axisLeft(yScale)
       .ticks(5);
     svg.append('g')
@@ -36,16 +48,18 @@ function App() {
     svg.append('g')
       .call(yAxis)
       .attr('id', 'y-axis');
+
     // 4 set up the svg data
     svg.selectAll('.bar')
-      .data(data)
+      .data(countryData)
       .join('rect')
         .attr('x', (v, i) => xScale(i))
         .attr('y', yScale)
         .attr('width', xScale.bandwidth())
         .attr('height', val => h - yScale(val))
         .attr('class', 'bar');
-  }, [data]);
+
+  }, [countryData]);
   //passing svgRef to svg lets svg control the DOM
   //react and D3 both control the DOM, so we are using useRef
   //to allow D3 to control the DOM.
