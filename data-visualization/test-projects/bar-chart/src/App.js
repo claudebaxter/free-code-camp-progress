113f224c31b0cabb1211d97005f1cac7f0b5259d
elborracho420 <e.baxter@dark-coin.io>
2023-02-13 22:32:35 -0600

    Accurately mapping GDP data to bar heights, rendering rough draft of bars in BarChart.

--- data-visualization/test-projects/bar-chart/src/App.js
+++ data-visualization/test-projects/bar-chart/src/App.js
@@ -1,76 +1,96 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
 import './App.css';
 
 function App() {
   const [countryData, setCountryData] = useState([]);
-  const svgRef = useRef();
 
-  //anything written in useEffect will be relevant to D3 code
   useEffect(() => {
-
-    //fetch JSON data
-    async function fetchData() {
-      const response = await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json");
-      const data = await response.json();
-      console.log(data);
-      setCountryData(data.data);
-    }
+  async function fetchData() {
+    const response = await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json");
+    const data = await response.json();
+    console.log("data", data.data);
+    setCountryData(data.data);
+  }
     fetchData();
-
-    console.log(countryData);
-
-    // 1 set up svg container
-    const w = 400;
-    const h = 300;
-    const svg = d3.select(svgRef.current)
-      .attr('width', w)
-      .attr('height', h)
-      .style('overflow', 'visible')
-      .style('margin-top', '75px');
-
-    // 2 set up scaling
-    const xScale = d3.scaleBand()
-      .domain(countryData.map((val, i) => i)) //maps x scale to show # of data points desired
-      .range([0, w])
-      .padding(0.5);
-    const yScale = d3.scaleLinear()
-      .domain([0, h])
-      .range([h, 0]); //inverting range, since y scale always starts from the top left
-
-    // 3 set up the axis
-    const xAxis = d3.axisBottom(xScale)
-      .ticks(countryData.length);
-    const yAxis = d3.axisLeft(yScale)
-      .ticks(5);
-    svg.append('g')
-      .call(xAxis)
-      .attr('transform', `translate(0, ${h})`)
-      .attr('id', "x-axis");
-    svg.append('g')
-      .call(yAxis)
-      .attr('id', 'y-axis');
-
-    // 4 set up the svg data
-    svg.selectAll('.bar')
-      .data(countryData)
-      .join('rect')
-        .attr('x', (v, i) => xScale(i))
-        .attr('y', yScale)
-        .attr('width', xScale.bandwidth())
-        .attr('height', val => h - yScale(val))
-        .attr('class', 'bar');
-
-  }, [countryData]);
-  //passing svgRef to svg lets svg control the DOM
-  //react and D3 both control the DOM, so we are using useRef
-  //to allow D3 to control the DOM.
+  }, []);
+  
   return (
     <div className="App">
       <h1 id="title">United States GDP</h1>
-      <svg ref={svgRef}></svg>
+      <BarChart data={countryData} height={500} widthOfBar={5} width={countryData.length * 5} dataType={"data"}/>
     </div>
   );
+}
+
+function BarChart ({data, height, width, widthOfBar, dataType}) {
+  useEffect(() => {
+    createBarChart();
+  }, [data])
+
+  const createBarChart = () => {
+    const countryData = data;
+
+    console.log("countryData", countryData);
+
+    const years = data.map(function (item) {
+      var quarter;
+      var temp = item[0].substring(5, 7);
+
+      if (temp === "01") {
+        quarter = "Q1";
+      } else if (temp === '04') {
+        quarter = 'Q2';
+      } else if (temp === '07') {
+        quarter = 'Q3';
+      } else if (temp === '10') {
+        quarter = 'Q4';
+      }
+
+      return item[0].substring(0, 4) + ' ' + quarter;
+    });
+
+    console.log("years", years);
+
+    var GDP = data.map(function (item) {
+      return item[1];
+    });
+
+    console.log("GDP", GDP);
+
+    var scaledGDP = [];
+
+    var gdpMax = d3.max(GDP);
+
+    console.log("gdpMax", gdpMax);
+
+    var linearScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
+
+    scaledGDP = GDP.map(function (item) {
+      return linearScale(item);
+    });
+    console.log("scaledGDP", scaledGDP);
+    //const dataMax = d3.max(countryData);
+    const yScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
+    d3.select("svg").selectAll("rect").data(countryData).enter().append("rect");
+    d3.select("svg")
+      .selectAll("rect")
+      .data(scaledGDP)
+      .style("fill", (d, i) => (i % 2 === 0 ? "#9595ff" : "44ff44"))
+      .attr("class", "bar")
+      .attr("x", (d, i) => i * widthOfBar)
+      .attr("y", (d) => height - yScale(d + gdpMax * 0.5))
+      .attr("height", (d, i) => yScale(d + gdpMax * 0.5))
+      .attr("width", widthOfBar);
+
+  }
+
+  return (
+    <>
+      <svg width={width} height={height}></svg>
+    </>
+  )
+
 }
 
 export default App;
