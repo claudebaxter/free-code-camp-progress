4488e8f9a1530a34e47abf915a1f10da07afc9db
elborracho420 <e.baxter@dark-coin.io>
2023-03-01 17:25:11 -0600

    Adding d3, setting up App.js structure and commenting todos.

--- data-visualization/test-projects/heat-map/src/App.js
+++ data-visualization/test-projects/heat-map/src/App.js
@@ -1,12 +1,71 @@
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json');
+      const data = await response.json();
+      setData(data);
+    }
+    console.log("data", data);
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1 id="title">Monthly Global Land-Surface Temperature</h1>
+      <div className="visHolder">
+        <Heatmap data={data}/>
+      </div>
     </div>
   );
+}
+
+function Heatmap ({ data }) {
+  const [height, setHeight] = useState(700);
+  const [width, setWidth] = useState(1000);
+  const [padding, setPadding] = useState(40);
+
+  useEffect(() => {
+    createHeatMap();
+  }, [data]);
+
+  const createHeatMap = () => {
+    console.log("data2", data);
+
+    //define axes scale xScale yScale
+
+    //define xAxis and yAxis
+
+    //add div for tooltip?
+
+    //define svg
+
+    //define gridlines
+
+    //svg.select(".x-axis & .y-axis")
+
+    //define data points (rectangles) on chart
+      //.on("mouseover / mouseout") functions for tooltip
+
+    //title & subtitle (svg.append("text"))
+
+    //let legendContainer = svg.append("g").attr("id", "legend");
+      //let legend = legendContainer.selectAll(#legend")....
+      //append legend rects
+      //append legend text
+  };
+
+  return (
+    <>
+      <svg width={width} height={height} padding={padding}></svg>
+    </>
+  )
+
 }
 
 export default App;
